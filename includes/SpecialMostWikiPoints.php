6f798be3b87f7ad8d6dff32e80475f147bd34ad6
SomeRandomDeveloper <thisisnotmyname275@gmail.com>
2025-09-17 18:23:19 +0200

    Fix a lot of phpcs errors

--- includes/SpecialMostWikiPoints.php
+++ includes/SpecialMostWikiPoints.php
@@ -1,52 +1,59 @@
 <?php
 namespace MediaWiki\Extension\WikiPoints;
-use MediaWiki\SpecialPage\SpecialPage;
-use MediaWiki\MediaWikiServices;
+
 use MediaWiki\Html\Html;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\SpecialPage\SpecialPage;
 use MediaWiki\Title\Title;
 
 class SpecialMostWikiPoints extends SpecialPage {
-    public function __construct() {
-        parent::__construct( 'MostWikiPoints' );
-        $this->MWServices = MediaWikiServices::getInstance();
-    }
 
-    public function execute( $subPage ) {
+	public function __construct() {
+			parent::__construct( 'MostWikiPoints' );
+			$this->MWServices = MediaWikiServices::getInstance();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute( $subPage ) {
 		$out = $this->getOutput();
 		$this->setHeaders();
-	    $dbProvider = $this->MWServices->getConnectionProvider();
-		$dbr = $dbProvider->getReplicaDatabase(); /** For MW < 1.40, use older method to get db connection **/
+		$dbProvider = $this->MWServices->getConnectionProvider();
+		$dbr = $dbProvider->getReplicaDatabase();
 		$qb = $dbr->newSelectQueryBuilder()
-		->select( [
-			'a.actor_name',
-			'wiki_points' => 'SUM( r.rev_len - COALESCE( p.rev_len, 0 ) )'
-		] )
-		->from( 'revision', 'r' )
-		->leftJoin( 'revision', 'p', [ 'r.rev_parent_id = p.rev_id' ] ) // table, alias, conds
-		->join( 'actor', 'a', [ 'r.rev_actor = a.actor_id' ] )         // table, alias, conds
-		->groupBy( [ 'r.rev_actor', 'a.actor_name' ] )
-		->orderBy( 'wiki_points', 'DESC' )
-		->limit( 20 )
-		->caller( __METHOD__ );
+			->select( [
+				'a.actor_name',
+				'wiki_points' => 'SUM( r.rev_len - COALESCE( p.rev_len, 0 ) )'
+			] )
+			->from( 'revision', 'r' )
+			// table, alias, conds
+			->leftJoin( 'revision', 'p', [ 'r.rev_parent_id = p.rev_id' ] )
+			// table, alias, conds
+			->join( 'actor', 'a', [ 'r.rev_actor = a.actor_id' ] )
+			->groupBy( [ 'r.rev_actor', 'a.actor_name' ] )
+			->orderBy( 'wiki_points', 'DESC' )
+			->limit( 20 )
+			->caller( __METHOD__ );
 		$res = $qb->fetchResultSet();
-        $out->addHTML( Html::openElement( 'table', [ 'class' => 'wikitable' ] ) );
-        $out->addHTML( Html::openElement( 'tr' ) );
-        $out->addHTML( Html::element( 'th', [], 'Rank' ) );
-        $out->addHTML( Html::element( 'th', [], 'Username' ) );
-        $out->addHTML( Html::element( 'th', [], 'WikiPoints' ) );
-        $out->addHTML( Html::closeElement( 'tr' ) );
+		$out->addHTML( Html::openElement( 'table', [ 'class' => 'wikitable' ] ) );
+		$out->addHTML( Html::openElement( 'tr' ) );
+		$out->addHTML( Html::element( 'th', [], 'Rank' ) );
+		$out->addHTML( Html::element( 'th', [], 'Username' ) );
+		$out->addHTML( Html::element( 'th', [], 'WikiPoints' ) );
+		$out->addHTML( Html::closeElement( 'tr' ) );
 		$i = 1;
 		$lang = $this->getLanguage();
-        $linkRenderer = $this->MWServices->getLinkRenderer();
+		$linkRenderer = $this->MWServices->getLinkRenderer();
 		foreach ( $res as $row ) {
-            $title = Title::newFromText( "Special:Contributions/{$row->actor_name}" );
-		    $out->addHTML( Html::openElement( 'tr' ) );
-            $out->addHTML( Html::element( 'td', [], $lang->formatNum( $i ) ) );
-            $out->addHTML( Html::rawElement( 'td', [], $linkRenderer->makeLink( $title, $row->actor_name, [] ) ) );
-            $out->addHTML( Html::element( 'td', [], $lang->formatNum( $row->wiki_points ) ) );
-            $out->addHTML( Html::closeElement( 'tr' ) );
+			$title = Title::newFromText( "Special:Contributions/{$row->actor_name}" );
+			$out->addHTML( Html::openElement( 'tr' ) );
+			$out->addHTML( Html::element( 'td', [], $lang->formatNum( $i ) ) );
+			$out->addHTML( Html::rawElement( 'td', [], $linkRenderer->makeLink( $title, $row->actor_name, [] ) ) );
+			$out->addHTML( Html::element( 'td', [], $lang->formatNum( $row->wiki_points ) ) );
+			$out->addHTML( Html::closeElement( 'tr' ) );
 			$i++;
-        }
-        $out->addHTML( Html::closeElement( 'table' ) );
+		}
+		$out->addHTML( Html::closeElement( 'table' ) );
 	}
 }
--- includes/SpecialWikiPoints.php
+++ includes/SpecialWikiPoints.php
@@ -1,36 +1,38 @@
 <?php
 namespace MediaWiki\Extension\WikiPoints;
-use MediaWiki\SpecialPage\SpecialPage;
-use HTMLForm;
-use MediaWiki\Title\Title;
-use MediaWiki\MediaWikiServices;
 
+use MediaWiki\HTMLForm\HtmlForm;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\SpecialPage\SpecialPage;
+use MediaWiki\Title\Title;
 
 class SpecialWikiPoints extends SpecialPage {
-    public function __construct() {
-        parent::__construct( 'WikiPoints' );
-        $this->MWServices = MediaWikiServices::getInstance();
-    }
 
-    public function execute( $subPage ) {
+	public function __construct() {
+		parent::__construct( 'WikiPoints' );
+		$this->MWServices = MediaWikiServices::getInstance();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function execute( $subPage ) {
 		$out = $this->getOutput();
 		$this->setHeaders();
 		$formDescriptor = [
-				'username' => [
-					'section' => 'wikipoints-special-form-name',
-					'label-message' => 'wikipoints-special-form-name-label',
-					'type' => 'user',
-				],
-			];
+			'username' => [
+				'section' => 'wikipoints-special-form-name',
+				'label-message' => 'wikipoints-special-form-name-label',
+				'type' => 'user',
+			],
+		];
 
 		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
 		$htmlForm
-		->setSubmitText( 'Submit' )
-		->setSubmitCallback( [ $this, 'trySubmit' ] )
-		->show();
+			->setSubmitText( 'Submit' )
+			->setSubmitCallback( [ $this, 'trySubmit' ] )
+			->show();
 		if ( $subPage ) {
-			$dbProvider = $this->MWServices->getConnectionProvider();
-			$dbr = $dbProvider->getReplicaDatabase();
 			$username = str_replace( '_', ' ', $subPage );
 			$userID = $this->getUserID( $username );
 			if ( $userID == 0 ) {
@@ -38,33 +40,35 @@ class SpecialWikiPoints extends SpecialPage {
 			} else {
 				$lang = $this->getLanguage();
 				$wikiPoints = $lang->formatNum( $this->calculateWikiPoints( $userID ) );
-				$out->addWikiTextAsContent( "[[Special:Contributions/$username|$username]] has '''$wikiPoints''' WikiPoints." );
+				$out->addWikiTextAsContent(
+					"[[Special:Contributions/$username|$username]] has '''$wikiPoints''' WikiPoints."
+				);
 			}
 		}
 	}
 
 	public function trySubmit( $formData ) {
-        $out = $this->getOutput();
+		$out = $this->getOutput();
 		$out->redirect( Title::makeTitleSafe( NS_SPECIAL, 'WikiPoints/' . $formData[ 'username' ] )->getLocalURL() );
 		return true;
 	}
 
 	private function getUserID( $user ) {
-        $userFactory = $this->MWServices->getUserFactory();
+		$userFactory = $this->MWServices->getUserFactory();
 		$userID = $userFactory->newFromName( $user )->getActorId();
 		return $userID;
 	}
-	
+
 	private function calculateWikiPoints( $userID ) {
 		$dbProvider = $this->MWServices->getConnectionProvider();
 		$dbr = $dbProvider->getReplicaDatabase();
-        $wikiPoints = $dbr->newSelectQueryBuilder()
-        ->select( [ 'wiki_points' => 'SUM( r.rev_len - COALESCE( p.rev_len, 0 ) )' ] )
-        ->from( 'revision', 'r' )
-        ->leftJoin( 'revision', 'p', 'r.rev_parent_id = p.rev_id' )
-        ->where( [ 'r.rev_actor' => $userID ] )
-        ->caller( __METHOD__ )
-        ->fetchRow()->wiki_points;
+		$wikiPoints = $dbr->newSelectQueryBuilder()
+			->select( [ 'wiki_points' => 'SUM( r.rev_len - COALESCE( p.rev_len, 0 ) )' ] )
+			->from( 'revision', 'r' )
+			->leftJoin( 'revision', 'p', 'r.rev_parent_id = p.rev_id' )
+			->where( [ 'r.rev_actor' => $userID ] )
+			->caller( __METHOD__ )
+			->fetchRow()->wiki_points;
 		return $wikiPoints;
 	}
 }
